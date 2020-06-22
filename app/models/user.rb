a3e9fd767e5f08b2181bebc4a7d57ef4a7690357
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-06-22 22:25:40 +0100

    Style all the webpages

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,26 +9,73 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  ## **********************************************************
 
   has_many :friendships, dependent: :destroy
+  # has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+
+  # Users who requested to be friends (needed for notifications )
+  has_many :inverted_friendships, -> { where confirmed: true }, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friends_requests, through: :inverted_friendships #class_name: 'Friendship'
+
+  # Users who need to confirm friendship
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :pending_friends, through: :pending_friendships, source: :friend
+
+  # Find user friends only by user_id from friendship table 
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
+  has_many :friends, through: :confirmed_friendships #class_name: 'Friendship'
+  
+
+  def friends_and_own_posts
+    Post.where(user: (self.friend + self))
+  end
+
+  # Users who have yet to confirm friend requests
+  def pending_friend?(user)
+    friend_requests.include?(user)
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverted_friendships.map { |friendship| 
+      friendship.user if !friendship.confirmed }.compact
+  end
+
+  # Method to check if a given user is a friend
+  def friend?(user)
+    friends.include?(user)
+  end
+  ##***********************************************************
+  # has_many :friendships, dependent: :destroy
   # has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
    # Users who requested to be friends (needed for notifications )
-  has_many :inverted_friendships, -> { where confirmed: true }, class_name: 'Friendship', foreign_key: 'friend_id'
-  has_many :friend_requests, through: :inverted_friendships, source: :user
+  # has_many :inverted_friendships, -> { where confirmed: true }, class_name: 'Friendship', foreign_key: 'friend_id'
+  # has_many :friend_requests, through: :inverted_friendships, source: :user
+
+  ####
+  # has_many :inverted_friendships, -> { where confirmed: true }, class_name: 'Friendship', foreign_key: 'friend_id'
+  # has_many :friends_requests, through: :inverted_friendships, source: :user
+  ####
  
    # Users who need to confirm friendship
-  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship'
-  has_many :pending_friends, through: :pending_friendships, source: :friend
+  # has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship'
+  # has_many :pending_friends, through: :pending_friendships, source: :friend
  
    # Find user friends only by user_id from friendship table 
-  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
-  has_many :friends, through: :confirmed_friendships, source: :friend
+  # has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
+  # has_many :friends, through: :confirmed_friendships, source: :friend
+
+  #####
+  # has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
+  # has_many :friends, through: :confirmed_friendships, class_name: 'Friendship'
+  ####
    
  
-   def friends_and_own_posts
-     Post.where(user: (self.friend + self))
-   end
+  #  def friends_and_own_posts
+    #  Post.where(user: (self.friend + self))
+  #  end
  
   #  def friends
   #    friends_array = friendships.map { |friendship| 
@@ -41,20 +88,20 @@ class User < ApplicationRecord
   #  end
  
    # Users who have yet to confirm friend requests
-   def pending_friend?(user)
-     friend_requests.include?(user)
-   end
+  #  def pending_friend?(user)
+    #  friend_requests.include?(user)
+  #  end
  
    # Users who have requested to be friends
   #  def friend_requests
-  #    inverted_friendships.map { |friendship| 
-  #      friendship.user if !friendship.confirmed }.compact
+    #  inverted_friendships.map { |friendship| 
+      #  friendship.user if !friendship.confirmed }.compact
   #  end
  
    # Method to check if a given user is a friend
-   def friend?(user)
-     friends.include?(user)
-   end
+  #  def friend?(user)
+    #  friends.include?(user)
+  #  end
 
   ## **********************************************************************************##
   ## **********************************************************************************##
