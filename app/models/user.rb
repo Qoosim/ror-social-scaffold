5344c1be5f5cfa70fb84edc7aee8776be80992b9
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-06-23 01:19:28 +0100

    Resolved conflict in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,14 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  ## **********************************************************
+
 
   has_many :friendships, foreign_key: 'user_id', dependent: :destroy
   # has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
 
   # Users who requested to be friends (needed for notifications )
   has_many :inverted_friendships, -> { where confirmed: true }, class_name: 'Friendship', foreign_key: 'friend_id'
-  has_many :friends_requests, through: :inverted_friendships #class_name: 'Friendship'
+  has_many :friends_requests, through: :inverted_friendships
 
   # Users who need to confirm friendship
   has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
@@ -24,8 +24,8 @@ class User < ApplicationRecord
 
   # Find user friends only by user_id from friendship table 
   has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
-  has_many :friends, through: :confirmed_friendships #class_name: 'Friendship'
-    
+  has_many :friends, through: :confirmed_friendships
+
 
   def friends_and_own_posts
     Post.where(user: (self.friend + self))
